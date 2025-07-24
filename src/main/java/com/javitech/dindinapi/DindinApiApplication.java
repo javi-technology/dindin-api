c505c94114d5663ff470f2642ab8d46aa7864242
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2025-07-23 20:54:48 -0400

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/javitech/dindinapi/DindinApiApplication.java
@@ -0,0 +1,13 @@
+package com.javitech.dindinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DindinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DindinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javitech/dindinapi/DindinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javitech.dindinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DindinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
