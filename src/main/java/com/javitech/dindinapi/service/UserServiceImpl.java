6c607292bb4ee3a7e5d86004a9a186ddb8d58499
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2025-09-11 16:42:11 -0300

    #1: implement method to retrieve approved users from the user repository

--- src/main/java/com/javitech/dindinapi/service/UserServiceImpl.java
+++ src/main/java/com/javitech/dindinapi/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.javitech.dindinapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -41,7 +42,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findByApproved() {
-        return List.of();
+        List<User> users = userRepository.findAll();
+        List<User> approvedUsers = new ArrayList<>();
+
+        for (User user : users) {
+            if (user.getIsApproved() != null && user.getIsApproved()) {
+                approvedUsers.add(user);
+            }
+        }
+
+        return approvedUsers;
     }
 
     @Override
